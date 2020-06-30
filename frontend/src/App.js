f23b6912be9c749cf711ba86e0b511297c04a0bd
Annie Liao <liaowow@gmail.com>
2020-06-30 12:10:05 -0400

    foundation done

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>
+          Baby Name Blossoms!
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
