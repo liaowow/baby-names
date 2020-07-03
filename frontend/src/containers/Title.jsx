3d1847cd0509f3f737c0dd7a5103e0294766c229
Annie Liao <liaowow@gmail.com>
2020-07-03 12:18:09 -0400

    updated font

--- frontend/src/containers/Title.jsx
+++ frontend/src/containers/Title.jsx
@@ -4,7 +4,7 @@ export default function Title() {
     return (
       <div className="col">
 				<h1>Baby Name Blossoms</h1>
-				<h3>Visualizing Popular Baby Names in the USA with Petals</h3>
+				<h3>Visualizing Popular Baby Names with Petals</h3>
 				<h4>created by annie liao. inspired by filmflowers. data by ssa.gov.</h4>
       </div>
     )
--- frontend/src/containers/Viz.jsx
+++ frontend/src/containers/Viz.jsx
@@ -1,26 +1,30 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import DrawFlowers from '../d3/DrawFlowers'
 
 export default function Viz() {
 
-  let data = require(`../data/d1990.json`)
+	const initialData = require(`../data/d2010.json`)
+	const [data, setData] = useState(initialData)
 
 	useEffect(() => {
 		DrawFlowers(data)
-	}, [])
-
-  const handleYear = (e) => {
-    let year = e.target.value
-		data = require(`../data/d${year}.json`)
+		console.log("inside useEffect")
+	}, [data])
+	
+	const handleDecadeChange = (e) => {
+		let year = e.target.value
+		let newData = require(`../data/d${year}.json`)
+		setData(newData)
 		console.log(data)
-  }
+	}
+	
 
 	return (
 		<div className="col">
 			<h3>Select Decade</h3>
-			<button onClick={handleYear} value="1990">1990s</button>
-			<button onClick={handleYear} value="2000">2000s</button>
-			<button onClick={handleYear} value="2010">2010~2018</button>
+			<button onClick={handleDecadeChange} value="1990">1990s</button>
+			<button onClick={handleDecadeChange} value="2000">2000s</button>
+			<button onClick={handleDecadeChange} value="2010">2010~2018</button>
 			<div className="viz"></div>
 		</div>
 	)
